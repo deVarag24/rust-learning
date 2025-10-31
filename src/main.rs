557212a756c082a709a198b38130e2f67fe21933
Varnit Agrawal <devarag2405@gmail.com>
2025-10-31 09:04:24 +0530

    start learning with is even function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
 fn main() {
-    println!("{}", is_even(31));
-}
-
-
-fn is_even(num: i32) -> bool {
-    if num % 2 == 0 {
-        return true;
-    }
-
-    return false;
+    println!("Hello, world!");
 }
